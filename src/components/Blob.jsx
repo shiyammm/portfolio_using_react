f292c2c12ae80f4bd89c7fc7fad9ea61e0c94409
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-02 23:40:00 +0530

    Update blob with animation

--- src/components/Blob.jsx
+++ src/components/Blob.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Blob = () => {
   return (
-    <div>Blob</div>
-  )
-}
+    <section className="absolute top-1/2 mt-[-1rem] left-1/2 -translate-x-1/2 -translate-y-1/2 z-[-10]">
+      <div className="blob text-white bg-gradient-to-r from-violet to-cyan w-[37rem] h-[37rem] rounded-full blur-[6rem] max-w-full md:max-w-[45rem]"></div>
+    </section>
+  );
+};
 
-export default Blob
+export default Blob;
