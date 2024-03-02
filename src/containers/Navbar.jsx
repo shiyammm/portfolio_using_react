24c90e7a2c0fbe11b083d0586abc322eb5dabda4
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-02 23:46:26 +0530

    Update Navbar with Menu component

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Menu from '../components/Menu';
 
-const Navebar = () => {
+const Navbar = () => {
   return (
-    <div>Navebar</div>
-  )
-}
+    <nav className="absolute top-0 left-0 right-0 flex items-center justify-between h-10 px-16 pt-14 bg-gray-800 text-white z-[99]">
+      <header className="font-roslindale-display text-3xl text-white z-[60]">
+        Shiyam
+      </header>
+      <div>
+        <Menu />
+      </div>
+    </nav>
+  );
+};
 
-export default Navebar
+export default Navbar;
