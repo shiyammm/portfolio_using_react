71e888b1a19a9ec33b295a79af06892c18675cba
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-02 23:41:24 +0530

    Update title with styles

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Title = () => {
-  return (
-    <div>Title</div>
-  )
-}
+const Title = ({ title }) => {
+  return <h2 className="text-[4rem] mt-10 font-roslindale-display">{title}</h2>;
+};
 
-export default Title
+export default Title;
