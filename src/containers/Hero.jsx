0b8a38af405f22e000becdc5da1e23a56dc1bde6
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-02 23:42:44 +0530

    Update Hero section with blob background

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Blob from '../components/Blob';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative z-10 flex items-center justify-center w-full h-screen">
+      <Blob />
+      <div>
+        <h1 className="leading-[11rem] font-roslindale-display text-[11rem] text-white">
+          Let's innovate <br />
+          <span className="flex items-center justify-center w-full h-full gap-8">
+            <span className="inline-block px-6 py-4 mt-6 text-2xl font-semibold tracking-wide text-black rounded-full font-acme bg-cyan">
+              digital experiences
+            </span>{' '}
+            together,
+          </span>
+        </h1>
+        <span className="text-[2rem] font-circular-book flex justify-center mt-10 text-white ">
+          crafting them with code
+        </span>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
