25f7a6e9363a844d2454d8b96fb174e276862df8
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-02 23:38:58 +0530

    Import containers to  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Navbar from './containers/Navbar';
+import Hero from './containers/Hero';
+import About from './containers/About';
+import Works from './containers/Works';
+import Contact from './containers/Contact';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className=" bg-gray">
+      <Navbar />
+      <Hero />
+      <About />
+      <Works />
+      <Contact />
+    </main>
+  );
 };
 
 export default App;
