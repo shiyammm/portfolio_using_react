80a7c0d1fad7a3c8a4d39e7ec9c97cc3f96260ee
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-04 23:20:46 +0530

    Update App.jsx with lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import Lenis from '@studio-freight/lenis';
 import Navbar from './containers/Navbar';
 import Hero from './containers/Hero';
 import About from './containers/About';
 import Works from './containers/Works';
 import Contact from './containers/Contact';
+import WhatIUse from './containers/WhatiUse';
 
 const App = () => {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
   return (
-    <main className=" bg-gray">
+    <main className="bg-gray">
       <Navbar />
       <Hero />
       <About />
       <Works />
+      <WhatIUse />
       <Contact />
     </main>
   );
