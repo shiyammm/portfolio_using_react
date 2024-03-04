24f24698fdf772a2dbdd6452ad04ca31d822b83d
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-04 23:32:58 +0530

    Refactor Navbar and Menu Components, Implement GSAP Click Animation
     - Removed unwanted styles from the Navbar and Menu components to improve code cleanliness and maintainability. - Integrated GSAP animation library to add a click animation effect to the Navbar and Menu elements, enhancing user interaction.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,61 +1,83 @@
-import React from 'react';
-import { useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import React, { useRef } from 'react';
+import { PiStarFourFill } from 'react-icons/pi';
 
-const Menu = () => {
-  const [toggle, setToggle] = useState(false);
+const Menu = (props) => {
+  const toggle = props.toggle;
+  const tl = useRef();
+  const menuRef = useRef();
+  const linksRef = useRef();
 
-  const handleMenu = () => {
-    setToggle(!toggle);
-  };
+  useGSAP(
+    () => {
+      tl.current = gsap.timeline({
+        paused: true,
+      });
+      tl.current.to(menuRef.current, {
+        right: 0,
+        duration: 1,
+      });
+      tl.current.from('.list', {
+        y: 60,
+        delay: 0.2,
+        stagger: 0.1,
+        opacity: 0,
+      });
+      tl.current.from(linksRef, {
+        y: 50,
+        opacity: 0,
+        stagger: 0.2,
+      });
+      if (toggle) {
+        tl.current.play();
+      } else {
+        tl.current.reverse();
+      }
+    },
+    { scope: menuRef },
+  );
+
   return (
-    <div>
+    <div
+      className="fixed top-0 flex flex-col items-center justify-center w-full h-screen right-[-100vw] bg-gray"
+      ref={menuRef}
+    >
+      <ul className="unordered font-roslindale-display text-[11rem] text-white text-center leading-[15rem]">
+        <li className="flex items-center justify-center text-center gap-14 list">
+          {' '}
+          <PiStarFourFill className="w-10 h-10 text-violet" /> Work
+        </li>
+        <li className="flex items-center justify-center text-center gap-14 list">
+          {' '}
+          <PiStarFourFill className="w-10 h-10 text-violet" /> Contact
+        </li>
+        <li className="flex items-center justify-center text-center gap-14 list">
+          {' '}
+          <PiStarFourFill className="w-10 h-10 text-violet" /> CV
+        </li>
+      </ul>
       <div
-        onClick={handleMenu}
-        className=" relative flex items-center gap-4 z-[60] bg-violet px-5 py-2 text-black font-semibold active:border-none rounded-full text-2xl"
+        className="flex items-center justify-between w-full px-16 text-xl text-white font-circular-medium "
+        ref={linksRef}
       >
-        <span className="mb-1">Menu</span>
-        <svg width="40" height="40" viewBox="0 0 100 100">
-          <path
-            className={`line line1 ${toggle && 'opened-line1'}`}
-            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
-          />
-          <path
-            className={`line line2 ${toggle && 'opened-line2'}`}
-            d="M 20,50 H 80"
-          />
-          <path
-            className={`line line3 ${toggle && 'opened-line3'}`}
-            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
-          />
-        </svg>
+        <a
+          href=""
+          className="text-white transition-all duration-300 ease-in-out group"
+        >
+          <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+            LinkedIn
+          </span>
+        </a>
+        <a
+          href=""
+          className="text-white transition-all duration-300 ease-in-out group"
+        >
+          <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+            Github
+          </span>
+        </a>
       </div>
-      {toggle && (
-        <div className="absolute w-full h-screen flex flex-col gap-[6rem] items-center justify-center z-[50] left-0 top-0">
-          <ul className=" font-roslindale-display text-[11rem] text-white text-center leading-[15rem]">
-            <li>Work</li>
-            <li>Contact</li>
-            <li>CV</li>
-          </ul>
-          <div className="flex items-center justify-between w-full px-16 text-xl text-white font-circular-medium ">
-            <a
-              href=""
-              className="text-white transition-all duration-300 ease-in-out group"
-            >
-              <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-                LinkedIn
-              </span>
-            </a>
-            <a
-              href=""
-              className="text-white transition-all duration-300 ease-in-out group"
-            >
-              <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-                Github
-              </span>
-            </a>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import Menu from '../components/Menu';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleMenu = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <nav className="absolute top-0 left-0 right-0 flex items-center justify-between h-10 px-16 pt-14 bg-gray-800 text-white z-[99]">
-      <header className="font-roslindale-display text-3xl text-white z-[60]">
-        Shiyam
-      </header>
-      <div>
-        <Menu />
+    <nav className="fixed w-full backdrop-blur-lg h-[6rem] text-white z-[99] bg-ed-500">
+      <div className="flex justify-between w-full mt-8 px-14">
+        <header className="font-roslindale-display text-4xl text-white z-[60]">
+          Shiyam
+        </header>
+        <div
+          onClick={handleMenu}
+          className=" relative flex items-center gap-4 z-[60] bg-violet px-5 py-1.5 text-black font-semibold active:border-none rounded-2xl text-2xl"
+        >
+          <span className="mb-1">{`${toggle ? 'Close' : 'Menu'}`}</span>
+          <svg width="40" height="40" viewBox="0 0 100 100">
+            <path
+              className={`line line1 ${toggle && 'opened-line1'}`}
+              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+            />
+            <path
+              className={`line line2 ${toggle && 'opened-line2'}`}
+              d="M 20,50 H 80"
+            />
+            <path
+              className={`line line3 ${toggle && 'opened-line3'}`}
+              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+            />
+          </svg>
+        </div>
       </div>
+      {toggle && <Menu toggle={toggle} />}
     </nav>
   );
 };
