4a55ef1b43407ff9baa32a0c8265a5f4daebd9c7
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-07 23:21:45 +0530

    Extend Custom Cursor Visibility to All Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Lenis from '@studio-freight/lenis';
 import Navbar from './containers/Navbar';
 import Hero from './containers/Hero';
 import About from './containers/About';
 import Works from './containers/Works';
-import Contact from './containers/Contact';
 import WhatIUse from './containers/WhatiUse';
+import gsap from 'gsap';
+import Cursor from './components/Cursor';
+import { useGSAP } from '@gsap/react';
+import Contact from './containers/Contact';
 
 const App = () => {
+  const [hoverLink, setHoverLink] = useState(false);
+  const [hoverText, setHoverText] = useState(false);
+  const [hoverIcon, setHoverIcon] = useState(false);
+
+  const cursor = useRef();
   const lenis = new Lenis();
 
   function raf(time) {
@@ -17,13 +25,36 @@ const App = () => {
 
   requestAnimationFrame(raf);
 
+  useGSAP(() => {
+    const mouse = (e) => {
+      gsap.to(cursor.current, {
+        x: e.x,
+        y: e.y,
+      });
+    };
+    window.addEventListener('pointermove', mouse);
+  });
+
   return (
-    <main className="bg-gray">
+    <main className="relative bg-gray">
+      <div ref={cursor} className="fixed z-[100]">
+        <Cursor
+          hoverLink={hoverLink}
+          hoverText={hoverText}
+          hoverIcon={hoverIcon}
+        />
+      </div>
       <Navbar />
       <Hero />
       <About />
-      <Works />
-      <WhatIUse />
+      <Works
+        cursor={cursor}
+        hoverLink={hoverLink}
+        hoverText={hoverText}
+        setHoverText={setHoverText}
+        setHoverLink={setHoverLink}
+      />
+      <WhatIUse hoverIcon={hoverIcon} setHoverIcon={setHoverIcon} />
       <Contact />
     </main>
   );
