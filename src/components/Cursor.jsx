a164386195ce950fefa97569129e4626e5a3724e
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-07 23:18:55 +0530

    Change name of ViewSite component to Cursor; Add hoverIcon feature

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TfiArrowTopRight } from 'react-icons/tfi';
 
-const ViewSite = ({ hoverLink, hoverText }) => {
+const Cursor = ({ hoverLink, hoverText, hoverIcon }) => {
   return (
     <div
       className={`fixed top-1/2 ${
@@ -9,10 +9,12 @@ const ViewSite = ({ hoverLink, hoverText }) => {
       } ${
         hoverText &&
         'scale-[4] backdrop-opacity-95 backdrop-invert bg-white/10 transition ease-in-out'
+      } ${
+        hoverIcon && 'scale-[4]  transition ease-in-out delay-150 bg-cyan'
       } left-1/2 -translate-x-1/2 pointer-events-none -translate-y-1/2 w-10 h-10 bg-cyan rounded-full flex items-center justify-center text-black text-[0.7rem] tracking-[0.1rem] font-gtApina font-bold`}
     >
       {hoverLink && <TfiArrowTopRight className="w-3 h-3" />}
     </div>
   );
 };
-export default ViewSite;
+export default Cursor;
