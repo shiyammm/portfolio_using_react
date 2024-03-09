410474f83562bdfdcde45ca204cf2556f73b48f9
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-09 22:55:23 +0530

    Revamp work section: Introduce large images, wrap headings, and enlarge text for improved presentation

--- src/containers/Works.jsx
+++ src/containers/Works.jsx
@@ -3,50 +3,64 @@ import { useRef } from 'react';
 import { projectsData } from '../../lib/data';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import ScrollTrigger from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/all';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Works = ({ cursor, setHoverLink, setHoverText }) => {
   const slider = useRef();
   const component = useRef();
+  const h2Ref = useRef();
 
+  const WorkHeadText1 = 'Digital Work';
+  const WorkHeadText2 = 'Presentation ';
+
+  const WorkHeadText1Words = WorkHeadText1.split('');
+  const WorkHeadText2Words = WorkHeadText2.split('');
   useGSAP(
     () => {
       /* window.innerWidth - to get the initial width of the screen
       scrollWidth - to get the width after finishing the end scroll of the slider
       then subtracting it to how much width to scroll to the x-axis
-    */
+   */
       function getScrollAmount() {
-        let workWidth = slider.current.scrollWidth;
-        let innerWidth = window.innerWidth;
-        let xOffset = innerWidth * 0.2; // 20% of innerWidth
-        let scrollAmount = -(workWidth - innerWidth + xOffset); // Adjusted scroll amount
-
-        return scrollAmount;
+        let racesWidth = slider.current.scrollWidth;
+        return -(racesWidth - window.innerWidth);
       }
 
-      gsap.fromTo(
-        slider.current,
-        { xPercent: 20 },
-        {
-          x: getScrollAmount,
-          duration: 5,
-          ease: 'none',
-          scrollTrigger: {
-            trigger: component.current,
-            start: 'top 0%',
-            // "+=" - the starting amount
-            end: () => `+=${getScrollAmount() * -1}`,
-            pin: true,
-            scrub: 1,
+      const tweenSlider = gsap.to(slider.current, {
+        x: getScrollAmount,
+        duration: 4,
+        ease: 'none',
+      });
 
-            // It'll get the width of the component for every screen-size refresh to be responsive for every devices
-            invalidateOnRefresh: true,
-            // markers: true,
-          },
-        },
-      );
+      ScrollTrigger.create({
+        trigger: component.current,
+        start: 'top 0%',
+        end: () => `+=${getScrollAmount() * -1}`,
+        pin: true,
+        animation: tweenSlider,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        // markers: true,
+      });
+
+      const tweenHead = gsap.from(['.headText1', '.headText2'], {
+        y: -100,
+        opacity: 0,
+        stagger: 5,
+        duration: 5,
+        ease: 'power3.inOut',
+      });
+
+      ScrollTrigger.create({
+        trigger: h2Ref.current,
+        start: 'top 60%',
+        end: 'top 30%',
+        animation: tweenHead,
+        scrub: 1,
+        // markers: true,
+      });
 
       const handleMouseEnter = () => {
         gsap.to(cursor.current, {
@@ -77,63 +91,68 @@ const Works = ({ cursor, setHoverLink, setHoverText }) => {
   };
 
   return (
-    <section
-      className="relative w-full h-screen overflow-hidden text-white"
-      ref={component}
-    >
-      <div className="fixed top-[9rem] text-white font-roslindale-display text-[7rem] left-14 z-[10]">
-        <span
-          onMouseEnter={handleHoverTextEnter}
-          onMouseLeave={handleHoverTextLeave}
-        >
-          Works →
-        </span>
-      </div>
-      <div className="flex items-end h-screen flex-nowrap work" ref={slider}>
+    <section ref={component} className="overflow-hidden bg-[#121315]">
+      <div ref={slider} className="flex h-screen w-fit flex-nowrap">
+        <div className="flex items-center justify-center w-screen h-screen">
+          <h2
+            className="text-[15rem] text-[#9b9d9a] uppercase font-BebasNeue-Regular font-medium leading-[13rem] tracking-[0.1rem] text-center"
+            ref={h2Ref}
+          >
+            {WorkHeadText1Words.map((WorkHeadText1Word, index) => (
+              <span key={index} className="inline-block headText1">
+                {WorkHeadText1Word}
+              </span>
+            ))}
+            <br />
+            {WorkHeadText2Words.map((WorkHeadText2Word, index) => (
+              <span
+                key={index}
+                className="text-[#eaeaea] inline-block headText2"
+              >
+                {WorkHeadText2Word}
+              </span>
+            ))}
+          </h2>
+        </div>
         {projectsData.map((project, index) => (
           <div
-            className="flex items-end w-full px-[5rem] py-[3rem] shrink-0 relative border-[em] border-red-500"
+            className="relative flex items-center justify-center w-screen h-screen space-x-[5rem] text-white"
             key={index}
           >
-            <div className="flex items-center w-full gap-[8rem] border-[em] border-green-500">
-              <div
-                className="w-1/2 min-h-[30rem]"
-                onMouseEnter={handleHoverLinkEnter}
-                onMouseLeave={handleHoverLinkLeave}
-              >
-                <img
-                  src={project.imageUrl}
-                  className="object-cover object-left w-full h-full transition-all"
-                  alt={project.title}
-                />
-              </div>
-              <div
-                className="flex flex-col w-1/2 gap-4"
-                onMouseEnter={handleHoverTextEnter}
-                onMouseLeave={handleHoverTextLeave}
-              >
-                <h3 className="text-[2rem] uppercase font-circular-book tracking-[0.2rem] font-bold text-white">
+            <a
+              href={`${project.link}`}
+              target="blank"
+              onMouseEnter={handleHoverLinkEnter}
+              onMouseLeave={handleHoverLinkLeave}
+            >
+              <img
+                src={project.imageUrl}
+                alt=""
+                className={`w-[57rem] h-[40rem] object-cover rounded-[2rem] ${
+                  index === 1
+                    ? 'object-left'
+                    : index === 2
+                    ? 'object-[40%]'
+                    : ''
+                }`}
+              />
+            </a>
+            <div className="h-[40rem] w-[30rem] flex flex-col gap-4">
+              <div className="absolute top-[14rem]">
+                <span className="text-[8rem] font-BebasNeue-Regular text-[#9b9d9a] leading-[7rem]">
+                  {String(index + 1).padStart(2, '0')}
+                </span>
+                <h3 className="text-[4rem] font-medium font-BebasNeue-Regular text-white">
                   {project.title}
                 </h3>
-                <p className="text-xl">{project.description}</p>
-                <div>
-                  <span className=" text-2xl font-semibold tracking-[0.1rem] font-neue-montreal">
-                    Features:
-                  </span>
-                  <ul className="list-disc text-lg pl-[2rem] py-5 font-circular-book">
-                    {project.features.map((features, index) => (
-                      <li key={index}>{features}</li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="flex gap-5">
-                  {project.tags.map((technology, index) => (
-                    <span
-                      className="px-4 py-2 font-bold text-black rounded-lg bg-cyan"
+                <div className="space-x-[1rem] pt-[2rem]">
+                  {project.tags.map((tech, index) => (
+                    <button
                       key={index}
+                      className="py-3 text-xl font-medium tracking-[0.1rem] text-white border-2 rounded-xl px-7 border-violet "
                     >
-                      {technology}
-                    </span>
+                      {tech}
+                    </button>
                   ))}
                 </div>
               </div>
