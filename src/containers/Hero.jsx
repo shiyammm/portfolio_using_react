d595056123c77b913329daaae438db0e263d0315
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-19 09:30:34 +0530

    Add parallax scroll

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Blob from '../components/Blob';
+import { useParallax } from 'react-scroll-parallax';
 
 const Hero = () => {
+  const { ref } = useParallax({ speed: -10 });
+
   return (
-    <section className="relative z-10 flex items-center justify-center w-full h-screen">
+    <section
+      className="relative z-10 flex items-center justify-center w-full h-screen hero"
+      ref={ref}
+    >
       <Blob />
       <div>
-        <h1 className="leading-[11rem] font-roslindale-display text-[11rem] text-white">
+        <h1 className="leading-[11rem] font-roslindale-display text-[10rem] xl:text-[8rem] xl:leading-[8rem] text-white">
           Let's innovate <br />
           <span className="flex items-center justify-center w-full h-full gap-8">
             <span className="inline-block px-6 py-4 mt-6 text-2xl font-semibold tracking-wide text-black rounded-full font-acme bg-cyan">
@@ -15,7 +21,7 @@ const Hero = () => {
             together,
           </span>
         </h1>
-        <span className="text-[2rem] font-circular-book flex justify-center mt-10 text-white ">
+        <span className="text-[2rem] xl:text-[1.5rem] font-circular-book flex justify-center mt-10 xl:mt-7 text-white ">
           crafting them with code
         </span>
       </div>
