5ca72ba515ccd06d32a279fdbc7d0c965d785831
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-28 17:39:06 +0530

    Resolve menu visibility issue and implement onclick animation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,97 +1,184 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { PiStarFourFill } from 'react-icons/pi';
 import { Link } from 'react-router-dom';
+import { MotionConfig, motion, easeInOut } from 'framer-motion';
+import { SiGithub } from 'react-icons/si';
+import { FaLinkedin } from 'react-icons/fa';
 
-const Menu = ({ toggle }) => {
-  const tl = useRef();
+const Menu = () => {
+  const [toggle, setToggle] = useState(false);
+
+  // const tl = useRef();
+  const tl2 = useRef();
   const menuRef = useRef();
+  const menuButton = useRef();
   const linksRef = useRef();
 
-  const NavLink1 = 'Works';
-  const NavLink2 = 'Contact';
-  const NavLink3 = 'CV';
-  const NavLinkWords1 = NavLink1.split('');
-  const NavLinkWords2 = NavLink2.split('');
-  const NavLinkWords3 = NavLink3.split('');
+  useEffect(() => {
+    const handleMenuClick = () => {
+      setToggle((prev) => !prev);
+    };
+    menuButton.current.addEventListener('click', handleMenuClick);
+    return () => {
+      menuButton.current.removeEventListener('click', handleMenuClick);
+    };
+  }, [toggle]);
 
-  useGSAP(() => {
-    tl.current = gsap.timeline({
-      paused: true,
-      reversed: true,
-    });
-    tl.current.fromTo(
-      menuRef.current,
-      {
-        x: '-100%',
-        duration: 0.5,
-      },
-      {
-        x: '0%',
-        duration: 0.5,
-      },
-    );
-    tl.current.from('.list', {
-      y: 60,
-      stagger: 0.1,
-      opacity: 0,
-    });
-    tl.current.from(linksRef.current, {
-      y: 50,
-      stagger: 0.2,
-      opacity: 0,
-    });
-  });
+  useGSAP(
+    () => {
+      tl2.current = gsap
+        .timeline({
+          paused: true,
+          reversed: true,
+        })
+        .from(menuRef.current, {
+          right: '5rem',
+          top: '2rem',
+          width: '0rem',
+          height: '0rem',
+          duration: 0.3,
+          ease: 'power.in',
+        })
+        .from('li', {
+          x: 500,
+          opacity: 0,
+          stagger: 0.2,
+          duration: 0.3,
+          ease: 'bounce.in',
+        })
+        .from(linksRef.current, {
+          y: 100,
+          opacity: 0,
+          duration: 0.5,
+          ease: 'power.in',
+        });
+    },
+    { scope: toggle },
+  );
 
   useEffect(() => {
-    toggle ? tl.current.play() : tl.current.reverse();
+    toggle ? tl2.current.play() : tl2.current.reverse();
   }, [toggle]);
 
   return (
-    <div
-      className="fixed top-0 flex-col flex-center w-full h-screen bg-black z-[300]"
-      ref={menuRef}
-    >
-      <ul className="unordered font-roslindale-display text-[11rem] text-white text-center leading-[15rem]">
-        <li className="text-center cursor-pointer hover:text-violet flex-center gap-14 list">
-          {' '}
-          <PiStarFourFill className="w-10 h-10 text-violet" />
-          <a to="/works">Work</a>
-        </li>
-        <li className="text-center cursor-pointer hover:text-violet flex-center gap-14 list">
-          {' '}
-          <PiStarFourFill className="w-10 h-10 text-violet" />
-          <a to="/contact">Contact</a>
-        </li>
-        <li className="text-center cursor-pointer hover:text-violet flex-center gap-14 list">
-          {' '}
-          <PiStarFourFill className="w-10 h-10 text-violet" />
-          <a to="/cv">CV</a>
-        </li>
-      </ul>
-      <div
-        className="flex items-center justify-between w-full px-16 text-xl text-white font-circular-medium "
-        ref={linksRef}
+    <>
+      <MotionConfig
+        transition={{
+          duration: 0.5,
+          ease: easeInOut,
+        }}
       >
-        <a
-          href=""
-          className="text-white transition-all duration-300 ease-in-out group"
+        <motion.button
+          initial={false}
+          animate={toggle ? 'open' : 'close'}
+          // style={{
+          //   width: 'calc(2rem + 2.5vw)',
+          //   height: 'calc(2rem + 2.5vh)',
+          // }}
+          className={`relative active:border-none rounded-full text-2xl w-14 h-14 md:w-[3.2rem] md:h-[3.2rem] cursor-pointer bg-white/30 backdrop-blur z-[400]`}
+          ref={menuButton}
         >
-          <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-            LinkedIn
-          </span>
-        </a>
-        <a
-          href=""
-          className="text-white transition-all duration-300 ease-in-out group"
-        >
-          <span className="bg-left-bottom bg-gradient-to-r from-white to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-            Github
-          </span>
-        </a>
+          <motion.span
+            style={{ left: '50%', top: '30%', x: '-50%', y: '-50%' }}
+            className="absolute h-[3px] bg-white w-7"
+            variants={{
+              open: {
+                rotate: ['0deg', '0deg', '45deg'],
+                top: ['33%', '33%', '50%'],
+              },
+              close: {
+                rotate: ['45deg', '0deg', '0deg'],
+                top: ['50%', '50%', '33%'],
+              },
+            }}
+          />
+          <motion.span
+            style={{
+              left: 'calc(50% + 4px)',
+              top: '50%',
+              x: '-50%',
+              y: '-50%',
+              width: '20px',
+            }}
+            className="absolute h-[3px] bg-white"
+            variants={{
+              open: {
+                rotate: ['0deg', '0deg', '-45deg'],
+                width: '28px',
+                left: '50%',
+              },
+              close: {
+                rotate: ['-45deg', '0deg', '0deg'],
+                width: '20px',
+                left: 'calc(50% + 4px)',
+              },
+            }}
+          />
+          <motion.span
+            style={{
+              left: 'calc(50% + 7px)',
+              bottom: '25%',
+              x: '-50%',
+              y: '-50%',
+            }}
+            className="w-3.5 h-[3px] bg-white absolute"
+            variants={{
+              open: {
+                rotate: ['0deg', '0deg', '45deg'],
+                bottom: ['27%', '27%', '50%'],
+                left: '45%',
+              },
+              close: {
+                rotate: ['45deg', '0deg', '0deg'],
+                bottom: ['50%', '50%', '27%'],
+                left: 'calc(50% + 7px)',
+              },
+            }}
+          />
+        </motion.button>
+      </MotionConfig>
+
+      <div
+        className="absolute top-0 bg-black h-screen w-[30rem] md:w-full z-[100] right-[0rem]"
+        ref={menuRef}
+      >
+        <div className="h-full flex-center">
+          <ul className="unordered font-neue-montreal text-[2.5rem] text-white text-left font-bold space-y-5">
+            <li className="cursor-pointer list">
+              <a to="/works">Home</a>
+            </li>
+            <li className="cursor-pointer list">
+              <a to="/works">Work</a>
+            </li>
+            <li className="cursor-pointer list">
+              <a to="/contact">Contact</a>
+            </li>
+            <li className="cursor-pointer list">
+              <a to="/cv">CV</a>
+            </li>
+          </ul>
+          <div
+            className="absolute flex items-center justify-between w-full px-16 text-xl text-white bottom-5 font-circular-book"
+            ref={linksRef}
+          >
+            <a href="" className="text-white">
+              <span className="flex items-center gap-5">
+                <SiGithub />
+                Github
+              </span>
+            </a>
+            <a href="" className="text-white">
+              <span className="flex items-center gap-5">
+                <FaLinkedin />
+                LinkedIn
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -5,8 +5,6 @@ import Magnet from '../components/Magnet';
 import { MotionConfig, motion, easeInOut } from 'framer-motion';
 
 const Navbar = ({ hoverNavLink, setHoverNavLink, navLoaderRef }) => {
-  const [toggle, setToggle] = useState(false);
-
   const navRef = useRef();
 
   let lastScroll = 0;
@@ -33,91 +31,18 @@ const Navbar = ({ hoverNavLink, setHoverNavLink, navLoaderRef }) => {
       className="fixed w-full text-white z-[200] top-0 transition-all"
       ref={navRef}
     >
-      <nav className="flex items-center justify-between w-full pt-6 px-14" ref={navLoaderRef} >
-        <Magnet>
-          <div className="text-[2rem] font-Canopee-Regular text-white z-[90]">
-            Shiyam Robert
-          </div>
-        </Magnet>
-        <MotionConfig
-          transition={{
-            duration: 0.5,
-            ease: easeInOut,
-          }}
+      <nav
+        className="flex items-center justify-between flex-shrink-0 w-full pt-6 px-14 md:px-8"
+        ref={navLoaderRef}
+      >
+        <div
+          className="text-[2rem] font-Canopee-Regular text-white"
+          style={{ fontSize: 'calc(1.5rem + 0.5vw)' }}
         >
-          <motion.button
-            initial={false}
-            animate={toggle ? 'open' : 'close'}
-            className={`relative z-[500] active:border-none rounded-full text-2xl cursor-pointer w-16 h-16 bg-white/30 backdrop-blur`}
-            onClick={() => setToggle((prev) => !prev)}
-          >
-            <motion.span
-              style={{ left: '50%', top: '30%', x: '-50%', y: '-50%' }}
-              className="absolute h-[3px] bg-white w-7"
-              variants={{
-                open: {
-                  rotate: ['0deg', '0deg', '45deg'],
-                  top: ['33%', '33%', '50%'],
-                },
-                close: {
-                  rotate: ['45deg', '0deg', '0deg'],
-                  top: ['50%', '50%', '33%'],
-                },
-              }}
-            />
-            <motion.span
-              style={{
-                left: 'calc(50% + 4px)',
-                top: '50%',
-                x: '-50%',
-                y: '-50%',
-                width: '20px',
-              }}
-              className="absolute h-[3px] bg-white"
-              variants={{
-                open: {
-                  rotate: ['0deg', '0deg', '-45deg'],
-                  width: '28px',
-                  left: '50%',
-                },
-                close: {
-                  rotate: ['-45deg', '0deg', '0deg'],
-                  width: '20px',
-                  left: 'calc(50% + 4px)',
-                },
-              }}
-            />
-            <motion.span
-              style={{
-                left: 'calc(50% + 7px)',
-                bottom: '25%',
-                x: '-50%',
-                y: '-50%',
-              }}
-              className="w-3.5 h-[3px] bg-white absolute"
-              variants={{
-                open: {
-                  rotate: ['0deg', '0deg', '45deg'],
-                  bottom: ['27%', '27%', '50%'],
-                  left: '45%',
-                },
-                close: {
-                  rotate: ['45deg', '0deg', '0deg'],
-                  bottom: ['50%', '50%', '27%'],
-                  left: 'calc(50% + 7px)',
-                },
-              }}
-            />
-          </motion.button>
-        </MotionConfig>
+          Shiyam Robert
+        </div>
+        <Menu />
       </nav>
-      <div className="relative">
-        <Menu
-          toggle={toggle}
-          hoverNavLink={hoverNavLink}
-          setHoverNavLink={setHoverNavLink}
-        />
-      </div>
     </header>
   );
 };
