117875d1f5b02cbb3aeb0bec8584019a7929d900
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-31 22:56:32 +0530

    Implement scroll timeline animation for skill icons on small screen devices

--- src/containers/WhatIUse.jsx
+++ src/containers/WhatIUse.jsx
@@ -3,11 +3,14 @@ import { PiStarFourFill } from 'react-icons/pi';
 import { useEffect, useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
 const WhatIUse = () => {
   const skillRef = useRef();
   const skillTextRef = useRef();
   const sliderRef = useRef();
+  const tl4 = useRef();
 
   const ParagraphText =
     'Crafting Exceptional Web Experiences with an Array of Essential Frontend Tools and Technologies to Deliver Engaging User Interactions and Seamless Digital Journeys.';
@@ -16,47 +19,62 @@ const WhatIUse = () => {
 
   useGSAP(
     () => {
-      gsap.from('.word', {
+      tl4.current = gsap.timeline();
+
+      tl4.current.from('.word', {
         y: 30,
         opacity: 0,
         stagger: 0.05,
         duration: 0.4,
         ease: 'power4.out',
-        scrollTrigger: {
-          trigger: skillRef.current,
-          start: 'top 40%',
-        },
       });
-      gsap.from('.options', {
+
+      tl4.current.from('.options', {
         y: 30,
         opacity: 0,
         duration: 0.4,
         ease: 'power4.out',
-        scrollTrigger: {
-          trigger: skillRef.current,
-          start: 'top 40%',
-        },
+      });
+
+      tl4.current.from('.icon', {
+        scale: 0,
+        opacity: 0,
+        rotate: 60,
+        x: -80,
+        stagger: 0.1,
+        ease: 'power1.inOut',
+      });
+
+      tl4.current.from('.name', {
+        scale: 0,
+        opacity: 0,
+        y: 80,
+        ease: 'power1.inOut',
+      });
+
+      gsap.to('.arrow-left', {
+        x: -10,
+        duration: 1,
+        ease: 'power1.inOut',
+        repeat: -1,
+        yoyo: true,
+      });
+      gsap.to('.arrow-right', {
+        x: 10,
+        duration: 1,
+        ease: 'power1.inOut',
+        repeat: -1,
+        yoyo: true,
+      });
+
+      ScrollTrigger.create({
+        trigger: skillRef.current,
+        start: 'top 40%',
+        animation: tl4.current,
       });
     },
     { scope: skillRef },
   );
-
-  useGSAP(() => {
-    gsap.to('.arrow-left', {
-      x: -10,
-      duration: 1,
-      ease: 'power1.inOut',
-      repeat: -1,
-      yoyo: true,
-    });
-    gsap.to('.arrow-right', {
-      x: 10,
-      duration: 1,
-      ease: 'power1.inOut',
-      repeat: -1,
-      yoyo: true,
-    });
-  });
 
   useEffect(() => {
     let skillTextWidth = skillTextRef.current.offsetWidth;
@@ -120,24 +138,24 @@ const WhatIUse = () => {
 
   return (
     <section
-      className=" w-full h-screen space-y-[8rem] overflow-hidden py-14"
+      className=" w-full h-screen space-y-[8rem] xl:space-y-[1rem] overflow-hidden py-14 xl:flex-center flex-col"
       ref={skillRef}
     >
       <div className="text-left text-white mt-[2rem] flex items-center justify-center title flex-col space-y-9 ">
-        <p className="text-[3rem] text-center  font-roslindale-display text-white/50  mt-[1rem] text-div px-10 skill-para">
+        <p className=" text-center  font-roslindale-display text-white/50  mt-[1rem] text-div px-10 skill-para">
           {ParagraphWords.map((ParagraphWord, index) => (
             <span key={index} className="inline-block mr-3 word">
               {ParagraphWord}
             </span>
           ))}
         </p>
-        <span className="flex items-center gap-3 text-white/50 font-circular-book options lg:hidden">
+        <span className="flex items-center gap-3 text-white/50 font-circular-book options xl:hidden">
           <span className="arrow-left"> ← </span>
-          Move your Cursor
+          <span>Move your Cursor</span>
           <span className="arrow-right"> → </span>
         </span>
       </div>
-      <div className="relative grid w-full lg:hidden " ref={sliderRef}>
+      <div className="relative grid w-full xl:hidden " ref={sliderRef}>
         <div className="skill_container" ref={skillTextRef}>
           <div className="flex flex-nowrap gap-[6rem] bg-violet font-Canopee-Regular skill_text uppercase p-7 2xl:p-4 xl:p-2 font-medium text-black tracking-[0.6rem] xl:mt-[3rem]">
             {skillsData.map((skill, index) => (
@@ -146,13 +164,47 @@ const WhatIUse = () => {
                 key={index}
               >
                 <PiStarFourFill className="w-10 h-10 font-Canopee-Regular" />
-                {skill}
+                {skill.name}
               </span>
             ))}
           </div>
         </div>
       </div>
-      <div></div>
+      <div
+        className="flex-wrap items-center justify-center hidden px-3 xl:flex"
+        style={{
+          width: 'calc(24.6rem + 24.6vw)',
+          // height: 'calc(20.5rem + 20.5vw)',
+          gap: 'calc(1.6rem + 1.6vw)',
+        }}
+      >
+        {skillsData.map((skill, i) => (
+          <div
+            style={{
+              width: 'calc(3.5rem + 3.5vw)',
+              height: 'calc(3.5rem + 3.5vw)',
+              // boxShadow: '0px 0px 5px 1px  #37EBA9',
+              // borderRadius: '50%',
+            }}
+            key={i}
+            className="flex flex-col items-center justify-center gap-5 text-center "
+          >
+            <skill.icon
+              className=" text-white/50 icon"
+              style={{
+                width: 'calc(2.1rem + 2.1vw)',
+                height: 'calc(2.1rem + 2.1vw)',
+              }}
+            />
+            <span
+              style={{ fontSize: 'calc(0.7rem + 0.7vw)' }}
+              className=" text-white/70 name"
+            >
+              {skill.name}
+            </span>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
