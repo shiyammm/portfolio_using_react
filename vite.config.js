6ba8085f8139fe467bd2e50719e6f21ad3453309
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-04-01 19:00:48 +0530

    Testing by updating vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@react-three/fiber', '@react-three/drei'],
-    },
-  },
 });
